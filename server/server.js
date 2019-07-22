d04ecfa5c0ed1eee713bd0f18e7822a0978d3c3b
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.wireless.rit.edu>
2019-07-22 16:10:35 -0400

    apollo-client has been setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://sk4248:J91r2aSni8Ajp6Yc@cluster0-k6nfz.mongodb.net/test?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
