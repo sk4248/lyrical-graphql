9e5c3e6a978fdab99bed385481484379fea0a5d5
sreekanth korampally <sreekanthkorampally@sreekanths-mbp.home>
2019-09-24 23:38:57 -0400

    small enhancement

--- client/components/App.js
+++ client/components/App.js
@@ -2,5 +2,8 @@ import React from 'react';
 
 
 export default ({children}) => {
-    return <div className = "container">{children}</div>
+    return <div className = "container">
+    <h1> Add lyrics to your thoughts </h1>
+    {children}
+    </div>
 };
